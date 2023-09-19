b2e114de355f7a341354a22fd2944d169315abf3
Anuj Gupta <anujgupta@windowslive.com>
2023-09-19 17:43:35 +0700

    add basic form

--- ChatGuPTa/ChatGuPTa/settings.py
+++ ChatGuPTa/ChatGuPTa/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'ChatGuPTa.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
