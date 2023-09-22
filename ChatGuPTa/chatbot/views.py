be744e20f902a4325e7a529e9222ed87e46d26d8
Anuj Gupta <anujgupta@windowslive.com>
2023-09-22 10:01:59 +0700

    add user auth

--- ChatGuPTa/chatbot/views.py
+++ ChatGuPTa/chatbot/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
 import openai
+
+from django.contrib import auth
+from django.contrib.auth.models import User
+
 
 import os
 from os.path import join, dirname
@@ -38,3 +42,44 @@ def chatbot(request):
     response = ask_openai(message)
     return JsonResponse({'message': message, 'response': response})
   return render(request, 'chatbot.html')
+
+def login(request):
+  if request.method == 'POST':
+    username = request.POST['username']
+    password = request.POST['password']
+    user = auth.authenticate(request, username=username, password=password)
+    if user is not None:
+      auth.login(request, user)
+      return redirect('chatbot')
+    else:
+      error_message = "INVALID USERNAME OR PASSWORD"
+      return render(request, 'login.html', {'error_message': error_message})
+  else:
+    return render(request, 'login.html')
+
+
+def register(request):
+  if request.method == "POST":
+    username = request.POST['username']
+    email = request.POST['email']
+    password1 = request.POST['password1']
+    password2 = request.POST['password2']
+
+    if password1 == password2:
+      try:
+        user = User.objects.create_user(username, email, password1)
+        user.save()
+        auth.login(request, user)
+        return redirect('chatbot')
+      except:
+        error_message = "ERROR CREATING ACCOUNT"
+        return render(request, 'register.html', {'error_message': error_message})
+    else:
+      error_message = "PASSWORDS DO NOT MATCH"
+      return render(request, 'register.html', {'error_message': error_message})
+
+  return render(request, 'register.html')
+
+
+def logout(request):
+  auth.logout(request)
